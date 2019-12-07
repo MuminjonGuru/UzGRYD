81bf8ce0a8e4dfb3832116438a10797fa69bdc1c
Abduraimov Muminjon <muminjonguru@outlook.com>
2019-12-07 13:23:09 +0500

    Add Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_200807) do
+ActiveRecord::Schema.define(version: 2019_12_07_075826) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.string "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2019_12_06_200807) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
